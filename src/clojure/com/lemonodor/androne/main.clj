35eb22d5f79f1cc380c61db79d766fb78ed97203
John Wiseman <jwiseman@morseproject.com>
2013-11-21 23:23:04 -0800

    Defined progress-bar.

--- src/clojure/com/lemonodor/androne/main.clj
+++ src/clojure/com/lemonodor/androne/main.clj
@@ -1,7 +1,27 @@
 (ns com.lemonodor.androne.main
   (:use [neko.activity :only [defactivity set-content-view!]]
         [neko.threading :only [on-ui]]
-        [neko.ui :only [make-ui]]))
+        [neko.ui :only [make-ui]]
+        [neko.ui.mapping :as mapping]
+        [neko.ui.traits :as traits]
+        )
+  (:import
+   (android.app Activity)
+   (android.widget ProgressBar)))
+
+
+(mapping/defelement :progress-bar
+  :classname ProgressBar
+  :inherits :view)
+
+(traits/deftrait :progress
+  [^ProgressBar wdg, {:keys [progress]} _]
+  (.setProgress wdg progress))
+
+(traits/deftrait :max
+  [^ProgressBar wdg, {:keys [max]} _]
+  (.setMax wdg max))
+
 
 (defactivity com.lemonodor.androne.AndroneActivity
   :def a
