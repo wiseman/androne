08d800cbfb80f7b0bf0a1fe07a070cac59fbaee0
John Wiseman <jwiseman@morseproject.com>
2013-11-21 23:06:26 -0800

    Added ar-drone.

--- project.clj
+++ project.clj
@@ -11,7 +11,8 @@
   :java-source-paths ["src/java" "gen"]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
 
-  :dependencies [[org.clojure-android/clojure "1.5.1-jb" :use-resources true]
+  :dependencies [[ar-drone "0.1.9a"]
+                 [org.clojure-android/clojure "1.5.1-jb" :use-resources true]
                  [neko/neko "3.0.0-preview3"]]
   :profiles {:dev {:dependencies [[android/tools.nrepl "0.2.0-bigstack"]
                                   [compliment "0.0.2"]]
