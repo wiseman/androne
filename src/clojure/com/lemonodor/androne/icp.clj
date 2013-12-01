55dbf3d77805457bd13e4a56b56fd0aa3c6707dc
John Wiseman <jwiseman@morseproject.com>
2013-12-01 00:51:36 -0800

    First pieces of ICP.

--- src/clojure/com/lemonodor/androne/icp.clj
+++ src/clojure/com/lemonodor/androne/icp.clj
@@ -1,3 +1,22 @@
 (ns com.lemonodor.androne.icp)
 
 
+(defn index-concept [concept frame]
+  (apply merge-with concat
+         (map #(into {} (map (fn [word] [(str word) [concept]]) %1))
+              (frame :index-sets))))
+
+(defn index-concepts [world]
+  (apply merge-with concat
+         (map #(apply index-concept %1) world)))
+
+(defn find-indices [world words]
+  [])
+
+(defn score-index-sets [world indices]
+  [])
+
+(defn icp [world words]
+  (-> world
+      (find-indices words)
+      score-index-sets))
